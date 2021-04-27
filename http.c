0688a389b8cb0ff87ad55e8acc75fdeec77526a8
Atif Afzal <aafzal@sumologic.com>
2021-04-28 00:14:39 +0530

    Add function for starts_with and trim_start

--- http.c
+++ http.c
@@ -1,19 +1,60 @@
+#include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int length(char *s) {
-    int l = 0;
-    while (*s != '\0') {
-        ++s;
-        ++l;
+struct request_response_type {
+    char* status;
+    char* headers;
+    char* html;
+};
+
+int starts_with(char* str, char* prefix) {
+    size_t len = strlen(str);
+    size_t prefixLen = strlen(prefix);
+
+    // not possible to have prefix longer than string
+    if (len < prefixLen) {
+        return 0;
     }
-    return l;
+
+    size_t j = 0;
+    while (j < prefixLen && prefix[j] == str[j]) {
+        ++j;
+    }
+
+    if (j == prefixLen) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void trim_start(char** str, char* prefix) {
+    size_t len = strlen(*str);
+    size_t prefixLen = strlen(prefix);
+
+    if (len < prefixLen) {
+        return;
+    }
+
+    size_t j = 0;
+    while (j < prefixLen && prefix[j] == (char)**str) {
+        ++(*str);
+        ++j;
+    }
+}
+
+void request(char* url, struct request_response_type** response) {
+    assert(starts_with(url, "http://"));
+    trim_start(&url, "http://");
+    printf("%s\n", url);
 }
 
 int main() {
     char url[] = "http://example.org/index.html";
+    struct request_response_type* response;
 
-    printf("%lu\n", sizeof(url));
+    request(url, &response);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
