16f5daa856cb588c5a4f7fdc988437d918c12023
Atif Afzal <aafzal@sumologic.com>
2021-04-29 18:35:15 +0530

    Add SDL2_ttf for font rendering

--- graphics.c
+++ graphics.c
@@ -1,47 +1,162 @@
-// SDL2 Hello, World!
-// This should display a white screen for 2 seconds
-// compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
-// run with: ./hello_sdl2
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    SDL_Window* window;  // Declare a pointer
+// Define MAX and MIN macros
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
-    SDL_Init(SDL_INIT_VIDEO);  // Initialize SDL2
+// Define screen dimensions
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow("An SDL2 window",         // window title
-                              SDL_WINDOWPOS_UNDEFINED,  // initial x position
-                              SDL_WINDOWPOS_UNDEFINED,  // initial y position
-                              800,                      // width, in pixels
-                              600,                      // height, in pixels
-                              SDL_WINDOW_OPENGL         // flags - see below
-    );
+#define FONT_PATH "JetBrainsMono-Regular.ttf"
 
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
+int main(int argc, char *argv[]) {
+    // Unused argc, argv
+    (void)argc;
+    (void)argv;
+
+    // Initialize SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf(
+            "SDL2 could not be initialized!\n"
+            "SDL2 Error: %s\n",
+            SDL_GetError());
+        return 0;
     }
 
-    // A basic main loop to prevent blocking
-    int is_running = 1;
-    SDL_Event event;
-    while (is_running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                is_running = 0;
+    // Initialize SDL2_ttf
+    TTF_Init();
+
+#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
+    // Disable compositor bypass
+    if (!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0")) {
+        printf("SDL can not disable compositor bypass!\n");
+        return 0;
+    }
+#endif
+
+    // Create window
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2_ttf sample", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf(
+            "Window could not be created!\n"
+            "SDL_Error: %s\n",
+            SDL_GetError());
+    } else {
+        // Create renderer
+        SDL_Renderer *renderer =
+            SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (!renderer) {
+            printf(
+                "Renderer could not be created!\n"
+                "SDL_Error: %s\n",
+                SDL_GetError());
+        } else {
+            // Declare rect of square
+            SDL_Rect squareRect;
+
+            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
+            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+
+            // Square position: In the middle of the screen
+            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+
+            TTF_Font *font = TTF_OpenFont(FONT_PATH, 40);
+            if (!font) {
+                printf(
+                    "Unable to load font: '%s'!\n"
+                    "SDL2_ttf Error: %s\n",
+                    FONT_PATH, TTF_GetError());
+                return 0;
             }
+
+            SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
+            SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
+            SDL_Texture *text = NULL;
+            SDL_Rect textRect;
+
+            SDL_Surface *textSurface = TTF_RenderText_Shaded(
+                font, "Red square", textColor, textBackgroundColor);
+            if (!textSurface) {
+                printf(
+                    "Unable to render text surface!\n"
+                    "SDL2_ttf Error: %s\n",
+                    TTF_GetError());
+            } else {
+                // Create texture from surface pixels
+                text = SDL_CreateTextureFromSurface(renderer, textSurface);
+                if (!text) {
+                    printf(
+                        "Unable to create texture from rendered text!\n"
+                        "SDL2 Error: %s\n",
+                        SDL_GetError());
+                    return 0;
+                }
+
+                // Get text dimensions
+                textRect.w = textSurface->w;
+                textRect.h = textSurface->h;
+
+                SDL_FreeSurface(textSurface);
+            }
+
+            textRect.x = (SCREEN_WIDTH - textRect.w) / 2;
+            textRect.y = squareRect.y - textRect.h - 10;
+
+            // Event loop exit flag
+            bool quit = false;
+
+            // Event loop
+            while (!quit) {
+                SDL_Event e;
+
+                // Wait indefinitely for the next available event
+                SDL_WaitEvent(&e);
+
+                // User requests quit
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+
+                // Initialize renderer color white for the background
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                // Clear screen
+                SDL_RenderClear(renderer);
+
+                // Set renderer color red to draw the square
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+                // Draw filled square
+                SDL_RenderFillRect(renderer, &squareRect);
+
+                // Draw text
+                SDL_RenderCopy(renderer, text, NULL, &textRect);
+
+                // Update screen
+                SDL_RenderPresent(renderer);
+            }
+
+            // Destroy renderer
+            SDL_DestroyRenderer(renderer);
         }
-        SDL_Delay(16);
+
+        // Destroy window
+        SDL_DestroyWindow(window);
     }
 
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
+    // Quit SDL2_ttf
+    TTF_Quit();
 
-    // Clean up
+    // Quit SDL
     SDL_Quit();
+
     return 0;
 }
