47e95c26d3fdd68c2c19aa15b3571e6e101d4390
Atif Afzal <aafzal@sumologic.com>
2021-04-30 23:41:18 +0530

    Early return in font example

--- graphics.c
+++ graphics.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
             "SDL2 could not be initialized!\n"
             "SDL2 Error: %s\n",
             SDL_GetError());
-        return 0;
+        return 1;
     }
 
     // Initialize SDL2_ttf
@@ -39,110 +39,111 @@ int main(int argc, char *argv[]) {
             "Window could not be created!\n"
             "SDL_Error: %s\n",
             SDL_GetError());
-    } else {
-        // Create renderer
-        SDL_Renderer *renderer =
-            SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if (!renderer) {
-            printf(
-                "Renderer could not be created!\n"
-                "SDL_Error: %s\n",
-                SDL_GetError());
-        } else {
-            // Declare rect of square
-            SDL_Rect squareRect;
+        return 1;
+    }
 
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+    // Create renderer
+    SDL_Renderer *renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf(
+            "Renderer could not be created!\n"
+            "SDL_Error: %s\n",
+            SDL_GetError());
+    }
+    // Declare rect of square
+    SDL_Rect squareRect;
 
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+    // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
+    squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+    squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
 
-            TTF_Font *font = TTF_OpenFont(FONT_PATH, 40);
-            if (!font) {
-                printf(
-                    "Unable to load font: '%s'!\n"
-                    "SDL2_ttf Error: %s\n",
-                    FONT_PATH, TTF_GetError());
-                return 0;
-            }
+    // Square position: In the middle of the screen
+    squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+    squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
 
-            SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
-            SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
-            SDL_Texture *text = NULL;
-            SDL_Rect textRect;
+    TTF_Font *font = TTF_OpenFont(FONT_PATH, 40);
+    if (!font) {
+        printf(
+            "Unable to load font: '%s'!\n"
+            "SDL2_ttf Error: %s\n",
+            FONT_PATH, TTF_GetError());
+        return 1;
+    }
 
-            SDL_Surface *textSurface = TTF_RenderText_Shaded(
-                font, "Red square", textColor, textBackgroundColor);
-            if (!textSurface) {
-                printf(
-                    "Unable to render text surface!\n"
-                    "SDL2_ttf Error: %s\n",
-                    TTF_GetError());
-            } else {
-                // Create texture from surface pixels
-                text = SDL_CreateTextureFromSurface(renderer, textSurface);
-                if (!text) {
-                    printf(
-                        "Unable to create texture from rendered text!\n"
-                        "SDL2 Error: %s\n",
-                        SDL_GetError());
-                    return 0;
-                }
+    SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
+    SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
+    SDL_Texture *text = NULL;
+    SDL_Rect textRect;
 
-                // Get text dimensions
-                textRect.w = textSurface->w;
-                textRect.h = textSurface->h;
+    SDL_Surface *textSurface = TTF_RenderText_Shaded(
+        font, "Red square", textColor, textBackgroundColor);
+    if (!textSurface) {
+        printf(
+            "Unable to render text surface!\n"
+            "SDL2_ttf Error: %s\n",
+            TTF_GetError());
+        return 1;
+    }
 
-                SDL_FreeSurface(textSurface);
-            }
+    // Create texture from surface pixels
+    text = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if (!text) {
+        printf(
+            "Unable to create texture from rendered text!\n"
+            "SDL2 Error: %s\n",
+            SDL_GetError());
+        return 1;
+    }
 
-            textRect.x = (SCREEN_WIDTH - textRect.w) / 2;
-            textRect.y = squareRect.y - textRect.h - 10;
+    // Get text dimensions
+    textRect.w = textSurface->w;
+    textRect.h = textSurface->h;
 
-            // Event loop exit flag
-            bool quit = false;
+    SDL_FreeSurface(textSurface);
 
-            // Event loop
-            while (!quit) {
-                SDL_Event e;
+    textRect.x = (SCREEN_WIDTH - textRect.w) / 2;
+    textRect.y = squareRect.y - textRect.h - 10;
 
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
+    // Event loop exit flag
+    bool quit = false;
 
-                // User requests quit
-                if (e.type == SDL_QUIT) {
-                    quit = true;
-                }
+    // Event loop
+    while (!quit) {
+        SDL_Event e;
 
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        // Wait indefinitely for the next available event
+        SDL_WaitEvent(&e);
 
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Draw text
-                SDL_RenderCopy(renderer, text, NULL, &textRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
-
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
+        // User requests quit
+        if (e.type == SDL_QUIT) {
+            quit = true;
         }
 
-        // Destroy window
-        SDL_DestroyWindow(window);
+        // Initialize renderer color white for the background
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+
+        // Set renderer color red to draw the square
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+        // Draw filled square
+        SDL_RenderFillRect(renderer, &squareRect);
+
+        // Draw text
+        SDL_RenderCopy(renderer, text, NULL, &textRect);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
     }
+
+    // Destroy renderer
+    SDL_DestroyRenderer(renderer);
+
+    // Destroy window
+    SDL_DestroyWindow(window);
 
     // Quit SDL2_ttf
     TTF_Quit();
