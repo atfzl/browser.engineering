f9b834191b263f360bf610796ac1d40eb85e49ee
Atif Afzal <aafzal@sumologic.com>
2021-05-01 12:47:25 +0530

    Better way to set dynamic string

--- str.c
+++ str.c
@@ -12,20 +12,10 @@ void strcat_resize(char** s1, const char* s2) {
 
 int main(int argc, char* argv[]) {
     char* s1 = malloc(6);
-    s1[0] = 'h';
-    s1[1] = 'e';
-    s1[2] = 'l';
-    s1[3] = 'l';
-    s1[4] = 'o';
-    s1[5] = '\0';
+    strcpy(s1, "hello");
 
     char* s2 = malloc(6);
-    s2[0] = 'w';
-    s2[1] = 'o';
-    s2[2] = 'r';
-    s2[3] = 'l';
-    s2[4] = 'd';
-    s2[5] = '\0';
+    strcpy(s2, "world");
 
     strcat_resize(&s1, s2);
 
