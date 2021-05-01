63b0d6991b53917d3ac8b17462426666982a3eb3
Atif Afzal <aafzal@sumologic.com>
2021-05-01 13:42:59 +0530

    Remove func

--- str.c
+++ str.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void strcat_resize(char** s1, const char* s2) {
-    *s1 = realloc(*s1, strlen(*s1) + strlen(s2) + 1);
-    strcat(*s1, s2);
-}
-
 int main(int argc, char* argv[]) {
     char* s1 = malloc(6);
     strcpy(s1, "hello");
@@ -14,7 +9,8 @@ int main(int argc, char* argv[]) {
     char* s2 = malloc(6);
     strcpy(s2, "world");
 
-    strcat_resize(&s1, s2);
+    s1 = realloc(s1, strlen(s1) + strlen(s2) + 1);
+    strcat(s1, s2);
 
     printf("%s\n", s1);
 
