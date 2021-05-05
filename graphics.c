21610146bcc72392d6f2a1291875b7ce1bc1c074
Atif Afzal <aafzal@sumologic.com>
2021-05-05 16:28:14 +0530

    Refactor

--- graphics.c
+++ graphics.c
@@ -47,17 +47,6 @@ int main(int argc, char *argv[]) {
             "SDL_Error: %s\n",
             SDL_GetError());
     }
-    // Declare rect of square
-    SDL_Rect squareRect;
-    {
-        // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-        squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-        squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-        // Square position: In the middle of the screen
-        squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-        squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-    }
 
     TTF_Font *font = TTF_OpenFont(FONT_PATH, 40);
     if (!font) {
@@ -102,11 +91,8 @@ int main(int argc, char *argv[]) {
 
     SDL_FreeSurface(textSurface);
 
-    // Event loop exit flag
-    bool quit = false;
-
     // Event loop
-    while (!quit) {
+    while (true) {
         SDL_Event e;
 
         // Wait indefinitely for the next available event
@@ -114,7 +100,7 @@ int main(int argc, char *argv[]) {
 
         // User requests quit
         if (e.type == SDL_QUIT) {
-            quit = true;
+            break;
         }
 
         // Initialize renderer color white for the background
@@ -122,9 +108,6 @@ int main(int argc, char *argv[]) {
 
         // Clear screen
         SDL_RenderClear(renderer);
-
-        // Set renderer color red to draw the square
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 
         // Draw text
         SDL_RenderCopy(renderer, text, NULL, &textRect);
