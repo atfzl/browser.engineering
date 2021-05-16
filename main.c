2a964d8e84be9ad28ce70ad4aec860aa946185ec
Atif Afzal <aafzal@sumologic.com>
2021-05-16 15:29:22 +0530

    Prepend DEBUG: to all DEBUG_INFO calls

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int ENABLE_DEBUG = 0;
 void init_debug() { ENABLE_DEBUG = (getenv("DEBUG") != NULL); }
 #define DEBUG_INFO(format, ...) \
-    if (ENABLE_DEBUG) fprintf(stdout, format, __VA_ARGS__)
+    if (ENABLE_DEBUG) fprintf(stdout, "DEBUG: " format, __VA_ARGS__)
 
 int main() {
     init_debug();
@@ -17,7 +17,7 @@ int main() {
 
     request(url, &response);
 
-    DEBUG_INFO("DEBUG: %s\n", url);
+    DEBUG_INFO("%s\n", url);
 
     const char* htmlText = lex(response.html);
 
