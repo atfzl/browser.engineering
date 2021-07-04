795af119c1f28f51fd45ebb11969de2ce0f5bcf4
Atif Afzal <aafzal@sumologic.com>
2021-07-04 23:15:51 +0530

    Fix clang tidy error

--- src/httpSSL.c
+++ src/httpSSL.c
@@ -12,12 +12,14 @@ httpSSL_t *httpSSL_init(int socketFD) {
 
   if (httpSSL->sslContext == NULL) {
     ERR_print_errors_fp(stderr);
+    free(httpSSL);
     return NULL;
   }
 
   httpSSL->ssl = SSL_new(httpSSL->sslContext);
   if (httpSSL->ssl == NULL) {
     fprintf(stderr, "SSL_new() failed\n");
+    free(httpSSL);
     return NULL;
   }
 
@@ -28,6 +30,7 @@ httpSSL_t *httpSSL_init(int socketFD) {
     SSL_get_error(httpSSL->ssl, status);
     ERR_print_errors_fp(stderr); // High probability this doesn't do anything
     fprintf(stderr, "SSL_connect failed with SSL_get_error code %d\n", status);
+    free(httpSSL);
     return NULL;
   }
 
